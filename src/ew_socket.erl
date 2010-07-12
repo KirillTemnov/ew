9f584e5f289cb345decb8d4a5c31b5719e4ffd6b
selead <allselead@gmail.com>
2010-07-12 19:04:54 +0400

    Remove unnecessary comments.

--- src/ew_socket.erl
+++ src/ew_socket.erl
@@ -59,20 +59,11 @@ start_link(LPid, LSocket, LPort) ->
 init({LPid, LSocket, LPort}) ->
     case catch gen_tcp:accept(LSocket) of
 	{ok, Socket} ->
-
-	    % io:fwrite("Address ~p Port ~p~n", [Addr, Port]),
-	    io:fwrite("Module = ~p~n", [?MODULE]),
-	    % io:fwrite("Accept socket HERE ~p~n", [Socket]),
-	    % io:fwrite("Lpid = ~p, Self pid = ~p~n", [LPid, self()]),
 	    ew_server:create(LPid, self()),
 	    io:fwrite("After FORK ~p~n", [Socket]),
-            % C = #c{sock = Socket,
-            %        port = ListenPort,
-            %        peer_addr = Addr,
-            %        peer_port = Port},
 	    {ok, {Addr, Port}} = inet:peername(Socket),
 	    Con = #connection{socket=Socket, port=LPort, peer_addr=Addr, peer_port=Port},
-	    get_request(Socket, Con, [], LPort); %% Jump to state 'request'
+	    get_request(Con, [], LPort); %% Jump to state 'request'
 	Error ->
 	    io:fwrite("Exit on error ~p~n", [Error]),
 	    exit({error, accept_failed})
@@ -83,25 +74,17 @@ init({LPid, LSocket, LPort}) ->
 %% Internal functions
 %%====================================================================
 
-
-
-% server() ->
-%     {ok, ListenSocket} = gen_tcp:listen(1234, [binary, {active, false}]),
-%     wait_connect(ListenSocket,0).
-
-% wait_connect(ListenSocket, Count) ->
-%     {ok, Socket} = gen_tcp:accept(ListenSocket), spawn(?MODULE, wait_connect, [ListenSocket, Count+1]), get_request(Socket, [], Count).
-
-get_request(Socket, Connection, BinaryList, Port) ->
+get_request(Connection, BinaryList, Port) ->
     io:fwrite("Get request~n", [ ]),
-    case gen_tcp:recv(Socket, 0, 10000) of
-	{ok, Binary} -> get_request(Socket, Connection, [Binary|BinaryList], Port);
+    case gen_tcp:recv(Connection#connection.socket, 0, ?MAX_IDLE_TIMEOUT) of
+	{ok, Binary} -> get_request(Connection, [Binary|BinaryList], Port);
 	{error, closed} ->
 	    handle(lists:reverse(BinaryList), Connection, Port);
 	{error, timeout} ->
 	    io:fwrite("Close request by timeout~n", [ ]),
 	    handle(lists:reverse(BinaryList), Connection, Port)
     end.
+
 
 
 handle(Binary, Connection, Port) ->
