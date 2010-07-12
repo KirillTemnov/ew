f9f050723aca27828556840ad1f0346e124a9eef
selead <allselead@gmail.com>
2010-07-12 15:26:06 +0400

    Initial commit

---
+++ src/ew_server.erl
@@ -0,0 +1,139 @@
+%%%-------------------------------------------------------------------
+%%% File    : ew_server.erl
+%%% Author  : selead <allselead@gmail.com>
+%%% Description : 
+%%%
+%%% Created : 12 Jul 2010 by selead <allselead@gmail.com>
+%%%-------------------------------------------------------------------
+-module(ew_server).
+
+-behaviour(gen_server).
+
+-define(TCP_OPTIONS,  {reuseaddr, true}, {active, false}, {backlog,30}).
+
+%%--------------------------------------------------------------------
+%% Include files
+%%--------------------------------------------------------------------
+
+%%--------------------------------------------------------------------
+%% External exports
+-export([start_link/1]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
+	 code_change/3, shutdown/1, create/2]).
+
+-record(state, {listen_socket, port, acceptor}).
+
+%%====================================================================
+%% External functions
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Function: start_link/0
+%% Description: Starts the server
+%%--------------------------------------------------------------------
+start_link(Port) when is_integer(Port) ->
+    Name = list_to_atom(lists:flatten(io_lib:format("ew_~p", [Port]))),
+    io:fwrite("GS start link~n", [ ]),
+    gen_server:start_link({local, Name}, ?MODULE, [Port], []).
+
+%%====================================================================
+%% Server functions
+%%====================================================================
+
+%% create new acceptor
+create(ServerPid, Pid) ->
+    io:fwrite("Before create!", [ ]),
+    gen_server:cast(ServerPid, {create, Pid}).
+
+shutdown(Port) ->
+    Name = list_to_atom(lists:flatten(io_lib:format("ew_~p", [Port]))),
+    io:fwrite("terminate ...~n", [ ]),
+    gen_server:cast({local, Name},terminate).
+
+%%--------------------------------------------------------------------
+%% Function: init/1
+%% Description: Initiates the server
+%% Returns: {ok, State}          |
+%%          {ok, State, Timeout} |
+%%          ignore               |
+%%          {stop, Reason}
+%%--------------------------------------------------------------------
+init([Port]) ->
+    process_flag(trap_exit, true),
+    io:fwrite("Gen server init~n", [ ]),
+    case gen_tcp:listen(Port, [binary, ?TCP_OPTIONS]) of
+	{ok, LSocket} ->
+	    %% create accepting process
+	    Pid = ew_socket:start_link(self(), LSocket, Port),
+	    io:fwrite("Create accepting process: (~p | ~p)~n", [LSocket, Port]),
+	    {ok, #state{listen_socket = LSocket, port = Port, acceptor = pid}};
+	{error, Reason} ->
+	    {stop, Reason}
+    end.
+
+%%--------------------------------------------------------------------
+%% Function: handle_call/3
+%% Description: Handling call messages
+%% Returns: {reply, Reply, State}          |
+%%          {reply, Reply, State, Timeout} |
+%%          {noreply, State}               |
+%%          {noreply, State, Timeout}      |
+%%          {stop, Reason, Reply, State}   | (terminate/2 is called)
+%%          {stop, Reason, State}            (terminate/2 is called)
+%%--------------------------------------------------------------------
+handle_call(Request, From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: handle_cast/2
+%% Description: Handling cast messages
+%% Returns: {noreply, State}          |
+%%          {noreply, State, Timeout} |
+%%          {stop, Reason, State}            (terminate/2 is called)
+%%--------------------------------------------------------------------
+%% Called by gen_server framework when the cast message from create/2 is received
+handle_cast({create, _Pid}, #state{listen_socket = LSocket} = State) ->
+    New_pid = ew_socket:start_link(self(), LSocket, State#state.port),
+    io:fwrite("Create new socket with pid = ~p~n", [New_pid]),
+    {noreply, State#state{acceptor=New_pid}};
+
+handle_cast(terminate, State) ->
+    {noreply, terminate(ok, State)};
+
+
+handle_cast(Msg, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: handle_info/2
+%% Description: Handling all non call/cast messages
+%% Returns: {noreply, State}          |
+%%          {noreply, State, Timeout} |
+%%          {stop, Reason, State}            (terminate/2 is called)
+%%--------------------------------------------------------------------
+handle_info(Info, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: terminate/2
+%% Description: Shutdown the server
+%% Returns: any (ignored by gen_server)
+%%--------------------------------------------------------------------
+terminate(Reason, State) ->
+    gen_tcp:close(State#state.listen_socket),
+    ok.
+
+
+%%--------------------------------------------------------------------
+%% Func: code_change/3
+%% Purpose: Convert process state when code is changed
+%% Returns: {ok, NewState}
+%%--------------------------------------------------------------------
+code_change(OldVsn, State, Extra) ->
+    {ok, State}.
+
+%%--------------------------------------------------------------------
+%%% Internal functions
+%%--------------------------------------------------------------------
---
+++ src/ew_socket.erl
@@ -0,0 +1,110 @@
+%%%-------------------------------------------------------------------
+%%% File    : ew_socket.erl
+%%% Author  : selead <allselead@gmail.com>
+%%% Description :
+%%%
+%%% Created : 12 Jul 2010 by selead <allselead@gmail.com>
+%%%-------------------------------------------------------------------
+-module(ew_socket).
+
+-compile(export_all).
+%%--------------------------------------------------------------------
+%% Include files
+%%--------------------------------------------------------------------
+
+%%--------------------------------------------------------------------
+%% External exports
+%%--------------------------------------------------------------------
+% -export([start_link/3
+%         ]).
+
+%%--------------------------------------------------------------------
+%% Internal exports
+%%--------------------------------------------------------------------
+% -export([init/1
+%         ]).
+
+%%--------------------------------------------------------------------
+%% Macros
+%%--------------------------------------------------------------------
+
+%%--------------------------------------------------------------------
+%% Records
+%%--------------------------------------------------------------------
+-record(connection, {socket, port}).
+
+-record(request, {connection=keep_alive,	        % keep_alive | close
+	      content_length,                   % Integer
+	      vsn,                              % {Maj,Min}
+	      method,                           % 'GET'|'POST'
+	      uri,				% Truncated URI /index.html
+              args="",                          % Part of URI after ?
+	      headers,				% [{Tag, Val}]
+	      body = <<>>}).			% Content Body
+
+
+%%====================================================================
+%% External functions
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Function:
+%% Description:
+%%--------------------------------------------------------------------
+start_link(LPid, LSocket, LPort) ->
+    proc_lib:spawn_link(?MODULE, init, [{LPid, LSocket, LPort}]).
+
+%% get from iserve_socket
+init({LPid, LSocket, LPort}) ->
+    case catch gen_tcp:accept(LSocket) of
+	{ok, Socket} ->
+	    {ok, {Addr, Port}} = inet:peername(Socket),
+	    io:fwrite("Address ~p Port ~p~n", [Addr, Port]),
+	    io:fwrite("Accept socket HERE ~p~n", [Socket]),
+	    %% create new listen socket
+	    %% iserve_server:create(Listen_pid, self()),
+	    io:fwrite("Lpid = ~p, Self pid = ~p~n", [LPid, self()]),
+	    ew_server:create(LPid, self()),
+	    io:fwrite("After FORK ~p~n", [Socket]),
+            % C = #c{sock = Socket,
+            %        port = ListenPort,
+            %        peer_addr = Addr,
+            %        peer_port = Port},
+	    Con = #connection{socket=Socket, port=LPort},
+	    get_request(Socket, [], 2); %% Jump to state 'request'
+	_ ->
+	    io:fwrite("Exit on error ~n", []),
+	    exit({error, accept_failed})
+    end.
+
+% request(Con, Req) ->
+%     Result = gen_tcp:recv(Con#connection.socket, 0),
+
+%     gen_tcp:close(Con#connection.socket).
+%%====================================================================
+%% Internal functions
+%%====================================================================
+
+
+
+% server() ->
+%     {ok, ListenSocket} = gen_tcp:listen(1234, [binary, {active, false}]),
+%     wait_connect(ListenSocket,0).
+
+% wait_connect(ListenSocket, Count) ->
+%     {ok, Socket} = gen_tcp:accept(ListenSocket), spawn(?MODULE, wait_connect, [ListenSocket, Count+1]), get_request(Socket, [], Count).
+
+get_request(Socket, BinaryList, Count) ->
+    io:fwrite("Get request~n", [ ]),
+    case gen_tcp:recv(Socket, 0, 10000) of
+	{ok, Binary} -> get_request(Socket, [Binary|BinaryList], Count);
+	{error, closed} ->
+	    handle(lists:reverse(BinaryList), Count);
+	{error, timeout} ->
+	    handle(lists:reverse(BinaryList), Count)
+    end.
+
+
+handle(Binary, Count) ->
+    {ok, Fd} = file:open("log_file_"++integer_to_list(Count), write),
+    file:write(Fd, Binary),
+    file:close(Fd).
