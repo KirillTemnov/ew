5cbc0e9d20f3d356b39a3a1b4b5bee83038a7996
selead <allselead@gmail.com>
2010-07-13 13:47:42 +0400

    Add sample test page, works fine.

--- src/ew_socket.erl
+++ src/ew_socket.erl
@@ -33,6 +33,9 @@
 -define(MAX_POST_TIMEOUT, 1000*30). %% 30 seconds
 
 -define(NOT_IMPLEMENTED, "HTTP/1.1 501 Not Implemented\r\n\r\n").
+-define(FORBIDDEN, "HTTP/1.1 403 Forbidden\r\n\r\n").
+-define(NOT_FOUND, "HTTP/1.1 404 Not Found\r\n\r\n").
+
 
 %%--------------------------------------------------------------------
 %% Records
@@ -68,7 +71,7 @@ init({LPid, LSocket, LPort}) ->
 	    io:fwrite("After FORK ~p~n", [Socket]),
 	    {ok, {Addr, Port}} = inet:peername(Socket),
 	    Con = #connection{socket=Socket, port=LPort, peer_addr=Addr, peer_port=Port},
-	    get_request(Con, #request{}, LPort); %% Jump to state 'request'
+	    get_request(Con, #request{}); %% Jump to state 'request'
 	Error ->
 	    io:fwrite("Exit on error ~p~n", [Error]),
 	    exit({error, accept_failed})
@@ -79,10 +82,10 @@ init({LPid, LSocket, LPort}) ->
 %% Internal functions
 %%====================================================================
 
-get_request(Connection, Request, Port) ->
+get_request(Connection, Request) ->
     io:fwrite("Get request~n", [ ]),
     case gen_tcp:recv(Connection#connection.socket, 0, ?MAX_IDLE_TIMEOUT) of
-%%	{ok, Binary} -> get_request(Connection, [Binary|BinaryList], Port);
+%%	{ok, Binary} -> get_request(Connection, [Binary|BinaryList]);
 
 	{ok, {http_request, Method, Path, Version}} ->
 	    get_headers(Connection, Request#request{vsn = Version,
@@ -90,25 +93,27 @@ get_request(Connection, Request, Port) ->
 						uri = Path}, []);
 
 	{error, {http_error, "\r\r"}} ->
-	    get_request(Connection, Request, Port);
+	    get_request(Connection, Request);
 	{error, {http_error, "\n"}} ->
-	    get_request(Connection, Request, Port);
+	    get_request(Connection, Request);
 	{error, closed} ->
-	    handle(lists:reverse(BinaryList), Connection, Port);
+	    close;
+%	    handle(lists:reverse(BinaryList), Connection, Port);
 	{error, timeout} ->
 	    io:fwrite("Close request by timeout~n", [ ]),
-	    handle(lists:reverse(BinaryList), Connection, Port)
+	    close
+%	    handle(lists:reverse(BinaryList), Connection, Port)
     end.
 
 get_headers(Connection, Request, HdrList) ->
     case gen_tcp:recv(Connection#connection.socket, 0, ?MAX_IDLE_TIMEOUT) of
 
 	{ok, {http_header, _, 'Host', _, Host}} ->
-	    get_headers(Connection, Request#request(host = Host), [{'Host', Host} | HdrList]);
+	    get_headers(Connection, Request#request{host = Host}, [{'Host', Host} | HdrList]);
 
 	{ok, {http_header, _, 'Content-Length', _, CLen}} ->
 	    Len = list_to_integer(CLen),
-	    get_headers(Connection, Request#request(content_length = Len),
+	    get_headers(Connection, Request#request{content_length = Len},
 			[{'Content-Length', Len} | HdrList]);
 
 	{ok, {http_header, _, 'Connection', _, Conn}} ->
@@ -142,12 +147,12 @@ keep_alive({1,1}, _)            -> keep_alive;
 keep_alive({1,0}, "Keep-Alive") -> keep_alive;
 keep_alive({1,0}, _)            -> close;
 keep_alive({0,9}, _)            -> close;
-keep_alive(_, _)                -> close; %% unknown values
+keep_alive(_, _)                -> close. %% unknown values
 
 
 %% get request body
 
-get_body(#connection{socket=Socket} = Connection, Request)->
+get_body(#connection{socket=Socket} = Connection, Request) ->
     case Request#request.method of
 	'GET' ->
 	    case handle_get(Connection, Request) of
@@ -176,14 +181,89 @@ get_body(#connection{socket=Socket} = Connection, Request)->
 	    exit(normal)
     end.
 
-handle_get(Connection, Request) ->
-    
-    ...
+handle_get(Connection, #request{connection=ConnState, host=Host} = Request) ->
+    case Request#request.uri of
+	{abs_path, FullPath} ->
+	    {Path, Args} = split_get_params(FullPath, []),
+	    call_mfa(Host,  Path, Args, Connection, Request),
+	    ConnState;
+	{absoluteURI, http, _Host, _, FullPath} ->
+	    {Path, Args} = split_get_params(FullPath, []),
+	    call_mfa(Host,  Path, Args, Connection, Request),
+	    ConnState;
+	{absoluteURI, _Other_method, _Host, _, FullPath} ->
+	    %% todo implement other methods
+	    send(Connection, ?NOT_IMPLEMENTED),
+	    close;
+	{absoluteURI, _Scheme, _RequestString} ->
+	    send(Connection, ?NOT_IMPLEMENTED),
+	    close;
+	_ ->
+	    send(Connection, ?FORBIDDEN),
+	    close
+    end.
 
 
-handle_post() ->
-    
-    ...
+%% implement post
+handle_post(Connection, #request{connection=ConnState, host=Host} = Request) ->
+    send(Connection, ?FORBIDDEN),
+    close.
+
+call_mfa(Host, Path, Args, Connection, Request) ->
+    Body =  list_to_binary(io_lib:format("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">
+<html>
+<head>
+  <title>ew test page</title>
+</head>
+<body>
+  <h2>Host: ~p </h2>
+  <h2>Path: ~p </h2>
+  <h2>Args: ~p </h2>
+</body>
+</html>", [Host, Path, Args])),
+    Headers = add_content_length([], Body),
+    Enc_headers = enc_headers(Headers),
+    Resp = [<<"HTTP/1.1 200 OK\r\n">>,
+	    Enc_headers,
+	    <<"\r\n">>,
+	    Body],
+    send(Connection, Resp).
+
+
+enc_headers([]) ->
+    [];
+enc_headers([{Tag, Val}|T]) when is_atom(Tag) ->
+    [atom_to_list(Tag), ": ", enc_header_val(Val), "\r\n"|enc_headers(T)];
+enc_headers([{Tag, Val}|T]) when is_list(Tag) ->
+    [Tag, ": ", enc_header_val(Val), "\r\n"|enc_headers(T)].
+
+
+
+enc_header_val(Val) when is_atom(Val) ->
+    atom_to_list(Val);
+enc_header_val(Val) when is_integer(Val) ->
+    integer_to_list(Val);
+enc_header_val(Val) ->
+    Val.
+
+
+add_content_length(Headers, Body) ->
+    case lists:keysearch('Content-Length', 1, Headers) of
+        {value, _} ->
+            Headers;
+        false ->
+            [{'Content-Length', size(Body)}|Headers]
+    end.
+
+
+%% cut off string after first question char ("?")
+split_get_params([], Acc) ->
+    {lists:reverse(Acc), []};
+split_get_params([$?|String], Acc) ->
+    {lists:reverse(Acc), String};
+split_get_params([Char|String], Acc) ->
+    split_get_params(String, [Char|Acc]).
+
 
 %% send data to the socket
 send(#connection{socket=Socket}, Data) ->
